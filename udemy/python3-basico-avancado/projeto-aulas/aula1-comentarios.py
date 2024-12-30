fc8195a53f6efbd221d5d397a539dcd5b437201a
Marlon Nezzo <nesso.narcizo@hotmail.com>
2024-12-30 11:00:09 -0400

    final class 6 python course

---
+++ udemy/python3-basico-avancado/projeto-aulas/aula1-comentarios.py
@@ -0,0 +1,17 @@
+""" -> DocString
+E escrever o que eu quiser
+
+Outro comentário no mesmo bloco
+"""
+
+'''-> Aspas simples (3) também servem como DocString
+
+Espaço pra adicionar qualquer texto
+
+Obs: O Python lê DocString, ela normalmente não é usada para comentários
+'''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(123)
---
+++ udemy/python3-basico-avancado/projeto-aulas/aula3-aspas.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Marlon Augusto')
+print(1, 'Marlon "Augusto"')
+
+# Aspas Duplas
+print("Marlon Augusto")
+print("Marlon 'Augusto'")
+
+# Escape
+print("Marlon \"Augusto\"")
+
+# r -> O criador da linguagem diz que isso é para expressões regulares
+print(r"Marlon \"Augusto\"")
