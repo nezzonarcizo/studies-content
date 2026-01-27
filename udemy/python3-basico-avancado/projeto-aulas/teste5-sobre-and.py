18e520290b7dc5fa1ad8aaec48ad69431ad4f507
Marlon Augusto <nesso.narcizo@hotmail.com>
2026-01-27 14:35:40 -0400

    Teste 6 - Operadores or e in

---
+++ udemy/python3-basico-avancado/projeto-aulas/teste5-sobre-and.py
@@ -0,0 +1,23 @@
+# 1 - Considerando o código a seguir:
+
+if 1 and 1:
+    print(True and 1 and False)
+
+# O que será exibido na tela?
+
+# R: False
+
+# 2 - Considerando o código a seguir:
+
+if 0 and 1:
+    print(True and 1)
+ 
+# O que será exibido na saída (tela)?
+
+# R: Nada
+
+
+
+
+
+
---
+++ udemy/python3-basico-avancado/projeto-aulas/teste6-or-in.py
@@ -0,0 +1,40 @@
+# Pergunta 1:
+# Qual o resultado do código abaixo?
+
+variavel_a = 1 or 0
+variavel_b = 0 or 1
+print(variavel_a, variavel_a)
+
+# R: 11
+
+# Pergunta 2:
+# Qual o resultado do código abaixo?
+
+nome = 'Maria Carmo'
+ 
+if ' ' in nome:
+    print(f'O nome {nome} tem espaços.')
+else:
+    print(f'O nome {nome} NÃO tem espaços.')
+
+# R: O nome Maria Carmo tem espaços.
+
+
+# Pergunta 3:
+# É possível adicionar um if dentro de outro fazendo várias condições aninhadas. Com isso em mente, 
+# o que você acha que o código abaixo exibe na tela?
+
+numero = 10
+ 
+if numero > 1:
+    if numero > 2:
+        if numero > 3:
+            print('Número maior que 3')
+        else:
+            print('Número menor que 3')
+    else:
+        print('Número menor que 2')
+else:
+    print('Número menor que 1')
+
+# R: Número maior que 3
